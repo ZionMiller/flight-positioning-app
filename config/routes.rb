e4d8db541e45798f74319f0c4def52db97313db4
Kevin Gicheha <gichehakevin@gmail.com>
2022-09-13 15:26:59 -0700

    working signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :flights
   resources :airports
   resources :users
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
