55543e0379aef9c95dae217aff044fdddd205c68
Kevin Gicheha <gichehakevin@gmail.com>
2022-09-12 21:15:14 -0700

    with updated tables

--- db/migrate/20220912232008_create_flights.rb
+++ db/migrate/20220912232008_create_flights.rb
@@ -2,8 +2,7 @@ class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
       t.integer :price
-      t.date :date
-      t.time :time
+      t.datetime :datetime
       t.string :departing_from
       t.string :destination
       t.string :layovers
