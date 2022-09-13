84498d130afdeca25153191587affef58ac6a411
Zion Miller <djzionmiller@gmail.com>
2022-09-12 22:59:30 -0700

    added some auth and validations in app controller, added show and create in users_controller, updated some cliet components

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,36 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authenticate_user
 
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
+    end
+
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    private
+
+    def unprocessable_entity(exception)
+        render json: { errors: exception.record.errors }, status: :unprocessable_entity
+    end
+
+    def not_found(error)
+        render json: { errors: {error.model => "Not found"}}, status: :not_found
+    end
+    # memoization, instantiate current user
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def authenticate_user
+        render json: { errors: {User: "You don't have acces"} }, status: :unathorized unless current_user
+    end
+
+    def authorized?
+        permitted = current_user.admin?
+        render json: { error: {User: "doesn't have permission"} }, status: :forbidden unless permitted
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    skip_before_action :authenticate_user, only: :create
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :zipcode, :username, :password)
+    end
+
 end
