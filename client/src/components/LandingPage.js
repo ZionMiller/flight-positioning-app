30dfb601a78145affbc3e9c725ca0dcb7756c432
Justin <jlmyers1993@hotmail.com>
2022-09-15 12:58:05 -0600

    Working on Landing Page and CSS

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,6 +7,29 @@ const LandingPage = () => {
         <>.....</>
         <h3>Welcome to our App </h3>
         <h3>Meet Our Developers</h3>
+        <div className="Container">
+           <div className="card">
+               <p>
+                   {/* <img className="AboutPicture" id="Zion" src={require()} alt="Zion"/> <br/> */}
+                   Zion Miller: <a href="https://github.com/ZionMiller">GitHub</a> - {''}
+                   <a href="https://www.linkedin.com/in/zion-miller-b15344170/">LinkedIn</a>
+               </p>
+           </div>
+           <div className="card">
+               <p>
+                   {/* <img className="AboutPicture" id="Kevin" src={require()} alt="Kevin"/> <br/> */}
+                   Kevin Gicheha: <a href="https://github.com/kgicheha">GitHub</a> - {''}
+                   <a href="https://www.linkedin.com/in/kevin-gicheha/">LinkedIn</a>         
+               </p>
+           </div>
+           <div className="card">
+               <p>
+                   {/* <img className="AboutPicture" id="Justin" src={require()} alt="Justin"/> <br/> */}
+                   Justin Myers: <a href="https://github.com/jlmyers14">GitHub</a> - {''}
+                   <a href="https://www.linkedin.com/in/justin-leroux-myers">LinkedIn</a>
+               </p>
+           </div>
+       </div>
     </>
   )
 }
