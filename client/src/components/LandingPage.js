d28263fc3e64001381916435780ba114843c41ae
Justin <jlmyers1993@hotmail.com>
2022-09-16 14:32:25 -0600

    Background image & some CSS

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,19 +3,20 @@ import React from 'react'
 const LandingPage = () => {
   return (
     <>
-    <h1>Flight Finder</h1>
-        <h2>.....</h2>
-        <h2>Click on the Calendar Above to Begin Searching for Cheap Flights</h2>
+    <h1><div id = "logo">
+    <img src={require("../Assets/logo.jpg")} alt="Logo" height="200"/>
+    </div></h1>
+        <h2>Log In or Sign Up to Begin Searching for Cheap Flights!</h2>
         <p>Welcome to our app! Our goal with this project is to help anyone get anywhere in the United States for the 
-            cheapest price possible. When traveling it can be such a hassle finding the right date and lowest price
+            cheapest price possible. When traveling it can be such a hassle finding the right date(s) and lowest price
             for your travels. With this app, we hope to cut out some of that headache for the consumer by doing the travel 
-            searching for them based off the dates selected. Then our app presents the user with multiple options of 
-            various routes and prices to choose from for their travels. </p>
+            searching for them based off the dates that are selected. Then our app presents the user with multiple options of 
+            various routes and prices to choose from for their travel needs. We hope you enjoy using it!</p>
         <h3>Meet Our Developers</h3>
         <div className="Container">
            <div className="card">
                <p>
-                   <img className="AboutPicture" id="Zion" src={require("../Assets/zion1.jpg")} alt="Zion"/> <br/>
+                   <img className="AboutPicture" id="Zion" src={require("../Assets/zion.jpg")} alt="Zion"/> <br/>
                    Zion Miller: <a href="https://github.com/ZionMiller">GitHub</a> - {''}
                    <a href="https://www.linkedin.com/in/zion-miller-b15344170/">LinkedIn</a>
                </p>
